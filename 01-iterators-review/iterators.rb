e4d75f2f82d0d517811bc8b0cba39d2298874ef7
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-07-13 12:24:56 -0400

    post lecture 01

--- 01-iterators-review/iterators.rb
+++ 01-iterators-review/iterators.rb
@@ -20,18 +20,57 @@ nums = (1..20).to_a #creates an array of all numbers from 1-20
 # 1.  Create an array of numbers where each new number is three times as big as
 # its original number (e.g., [3, 6, 9])
 
+nums_times_three = nums.map {|num| num * 3}
+
+# print nums_times_three
 
 # 2.  Find the first number that is divisible by 7
 
+divis_by_7 = nums.find do |num|
+  num % 7 == 0
+end
+
+# puts divis_by_7
 
 # 3.  Find all numbers that are divisible by 7
 
+divis_by_7 = nums.select do |num|
+  num % 7 == 0
+end
+
+# puts divis_by_7
 
 # 4.  Find the first number that is divisible by 7 AND greater than 10
 
+big_divis = nums.find do |num|
+  num % 7 == 0 && num > 10
+end
+
+# puts big_divis
 
 # Bonus:
 
 
 # 5.  Create an array of the squares (the number times itself) of all numbers
 # that are divisible by 7
+
+squares = nums.find_all do |num|
+  num % 7 == 0
+end.map do |seven|
+  seven ** 2
+end
+
+print squares
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dc-web-071320
 
+.each
+
+.map / collect
+
+.find / detect
+
+.select / find_all
