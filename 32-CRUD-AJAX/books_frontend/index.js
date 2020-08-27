857a99bd653c1fc282dcc3851f1900cd9e4ad247
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-08-27 13:19:57 -0500

    done

--- 32-CRUD-AJAX/books_frontend/index.js
+++ 32-CRUD-AJAX/books_frontend/index.js
@@ -1,3 +1,5 @@
+let url = "http://localhost:3000/books/" // "/books/book.id"
+
 // Append single book on DOM
 function addBook(book){
   
@@ -13,11 +15,72 @@ function addBook(book){
   const p = document.createElement("p")
   p.innerText = book.author
 
-  div.append(img, h3, p)
+  const btn = document.createElement("button")
+  if(book.read){
+    btn.innerText = "Mark Unread"
+  }
+  else{
+    btn.innerText = "Mark Read"
+  }
+
+  // read/unread a book => patch
+  btn.addEventListener("click", () => {
+    //url+book.id
+    //`http://localhost:3000/books/${book.id}`
+    // url + `${book.id}`
+    let configObj = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        read: !book.read
+      })
+    }
+
+    fetch(url+book.id, configObj)
+    .then(res => res.json())
+    .then(updatedBook => {
+      // if(updatedBook.read){
+      //   btn.innerText = "Mark Unread"
+      // }
+      // else{
+      //   btn.innerText = "Mark Read"
+      // }
+      updatedBook.read ? btn.innerText = "Mark Unread" : btn.innerText = "Mark Read"
+      // condition ? true : false
+    })
+
+
+  })
+
+  // Delete Btn
+
+  const delBtn = document.createElement("button")
+  delBtn.innerText = "Delete"
+
+  delBtn.addEventListener("click", () => {
+    deleteBook(book, div)
+    // fetch(url+book.id, {
+    //   method: "DELETE"
+    // })
+    // // .then(res => res.json())
+    // .then(() => div.remove())
+  })
+
+  div.append(img, h3, p, btn, delBtn)
 
   const book_div = document.querySelector("div#book-list") 
 
   book_div.append(div)
+}
+
+function deleteBook(book, div){
+  fetch(url+book.id, {
+    method: "DELETE"
+  })
+  // .then(res => res.json())
+  .then(() => div.remove())
 }
 
 // Iterating over an array
@@ -35,3 +98,49 @@ function getBooks(){
 }
 
 getBooks()
+
+// create a book
+// 1. select form and attach an eventListener
+// 2. Post request with user inputs
+// 3. Manupilate the DOM
+
+
+let form = document.querySelector("form#new-book")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  // event.preventDefault()
+
+  // debugger
+  let title = e.target[0].value
+  // let title = document.querySelector("input#book-title").value
+  let author = e.target[1].value
+  let cover = e.target[2].value
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      title: title,
+      author: author,
+      img: cover,
+      read: false
+    })
+  }
+
+  fetch(url, configObj)
+  .then(res => res.json())
+  .then(newBook => {
+    addBook(newBook)
+    // event.target.reset() => not going to work
+    form.reset() //pessimistic rendering
+    // e.target.reset()
+  })
+
+  // form.reset() //optimistic rendering 
+
+
+})
