8dff4978a8dbc0acabcb808a1f17929a34341178
Princeton Rose <princetonjrose@gmail.com>
2020-09-21 13:46:39 -0400

    added in react pizza breakout

---
+++ 44-React-Pizza/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Header = () => {
+
+  return (
+    <Fragment>
+      <h1 className="text-center">Welcome to Flatiron School's Pizzeria</h1>
+    </Fragment>
+  );
+
+}
+
+export default Header;
---
+++ 44-React-Pizza/src/components/Pizza.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Pizza = ( props ) => {
+
+  const { topping, size, vegetarian } = props.pizza
+
+  return(
+    <tr>
+      <td>{ topping }</td>
+      <td>{ size }</td>
+      <td>{ vegetarian ? 'Yes' : 'No' }</td>
+      <td><button
+            type="button"
+            className="btn btn-primary"
+            onClick={ () => props.clickPizza( props.pizza ) }
+          >Edit Pizza</button></td>
+    </tr>
+  )
+}
+
+export default Pizza
